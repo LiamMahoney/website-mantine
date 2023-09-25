0abc955d42c14d6c92e1213e92657aaef73021de
Liam Mahoney <liammahoney96@gmail.com>
2023-09-24 21:28:07 -0500

    init

--- components/ColorSchemeToggle/ColorSchemeToggle.tsx
+++ components/ColorSchemeToggle/ColorSchemeToggle.tsx
@@ -1,15 +1,49 @@
 'use client';
 
-import { Button, Group, useMantineColorScheme } from '@mantine/core';
+import { useState, useContext } from 'react';
+import { Switch, useMantineColorScheme } from '@mantine/core';
+import { IconSunFilled, IconMoonFilled } from '@tabler/icons-react';
+import classes from './ColorSchemeToggle.module.css';
+import { HydrationContext } from '@/src/context/HydrationContext';
 
 export function ColorSchemeToggle() {
-  const { setColorScheme } = useMantineColorScheme();
+    const { toggleColorScheme, colorScheme} = useMantineColorScheme();
+    const [ checked, setChecked ] = useState(colorScheme === 'dark');
+    const hydrated = useContext(HydrationContext);
 
-  return (
-    <Group justify="center" mt="xl">
-      <Button onClick={() => setColorScheme('light')}>Light</Button>
-      <Button onClick={() => setColorScheme('dark')}>Dark</Button>
-      <Button onClick={() => setColorScheme('auto')}>Auto</Button>
-    </Group>
-  );
+    function handleCheck(event:any) {
+        setChecked(event.currentTarget.checked);
+        toggleColorScheme();
+    }
+
+    if (hydrated) {
+        return (
+            <Switch
+                offLabel={<IconSunFilled height="80%" />}
+                onLabel={<IconMoonFilled height="80%" />}
+                size="md"
+                color="gray"
+                checked={checked}
+                onChange={handleCheck}
+                classNames={{
+                    trackLabel: classes.ColorSchemeToggleTrack
+                }}
+            />
+        );
+    } else {
+        return (
+            <Switch
+                offLabel={<IconSunFilled height="80%" />}
+                onLabel={<IconMoonFilled height="80%" />}
+                size="md"
+                color="gray"
+                checked={true}
+                onChange={handleCheck}
+                classNames={{
+                    trackLabel: classes.ColorSchemeToggleTrack
+                }}
+            />
+        )
+    }
+    
 }
---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import { useDisclosure } from '@mantine/hooks';
+import { Button, AppShell, Group, Burger, UnstyledButton, Stack, Flex } from '@mantine/core';
+import classes from './Navbar.module.css';
+import { ColorSchemeToggle } from '../ColorSchemeToggle/ColorSchemeToggle';
+
+export function Navbar({ children }: { children: any }) {
+    const [opened, { toggle }] = useDisclosure();
+
+    return (
+        <AppShell
+            header={{ height: 60 }}
+            aside={{ width: 300, breakpoint: 'sm', collapsed: { desktop: true, mobile: !opened } }}
+            padding="md"
+            transitionDuration={275} 
+        >
+            <AppShell.Header>
+                <Group h="100%" px="md">
+                    <Group className={classes.navContainer} >
+                        <Group className={classes.navLeft}>
+                            
+                        </Group>
+                        <Group className={classes.navCenter} gap={0} visibleFrom="sm" >
+                            <UnstyledButton className={classes.link}>Home</UnstyledButton>
+                            <UnstyledButton className={classes.link}>Projects</UnstyledButton>
+                            <UnstyledButton className={classes.link}>Blog</UnstyledButton>
+                        </Group>
+                        <Group className={classes.navRight}>
+                            <Group visibleFrom="sm">
+                                <ColorSchemeToggle />
+                            </Group>
+                            <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+                        </Group>
+                    </Group>
+                </Group>
+            </AppShell.Header>
+
+            <AppShell.Aside py="md" px={4} >
+                <Stack className={classes.mobileContainer}>
+                    <Stack className={classes.mobileLinkContainer}>
+                        {/* TODO: figure out what to do with mobile links. */}
+                        <UnstyledButton className={classes.link}>Home</UnstyledButton>
+                        <UnstyledButton className={classes.link}>Projects</UnstyledButton>
+                        <UnstyledButton className={classes.link}>Blog</UnstyledButton>
+                    </Stack>
+                    <Flex className={classes.mobileColorSchemeToggle} justify="flex-end" align="center">
+                        {/* TODO: increase size on mobile */}
+                        <ColorSchemeToggle />
+                    </Flex>
+                </Stack>
+
+            </AppShell.Aside>
+
+            <AppShell.Main>
+                {children}
+            </AppShell.Main>
+        </AppShell>
+    );
+}
