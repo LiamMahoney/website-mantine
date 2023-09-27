66f8db3ca6de791e4ebcfeb063562da287aaca7d
Liam Mahoney <liammahoney96@gmail.com>
2023-09-27 00:12:27 -0500

    adds favicon
    * adds favicon
    
    * removes icon-mask
    
    * adds mask-icon back
    
    * moves viewport towards top of head
    
    * udpates mask-icon path
    
    * removes mask-icon
    
    * moves viewport to bottom
    
    * fixes deployed styling issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,11 +10,13 @@ export default function RootLayout({ children }: { children: any }) {
         <html lang="en">
             <head>
                 <ColorSchemeScript />
-                <link rel="shortcut icon" href="/favicon.svg" />
-                <meta
-                    name="viewport"
-                    content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
-                />
+
+                <link rel="icon" href="/favicon.ico" sizes="32x32" />
+                <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+                <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+                <link rel="manifest" href="/manifest.webmanifest" />
+
+                <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"/>
             </head>
             <body>
                 <MantineProvider
