7b10e054995cc4e883673b501d3ed82a8f8eee7f
Liam Mahoney <liammahoney96@gmail.com>
2023-09-26 23:35:52 -0500

    moves viewport towards top of head

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,13 +11,13 @@ export default function RootLayout({ children }: { children: any }) {
             <head>
                 <ColorSchemeScript />
 
+                <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"/>
+
                 <link rel="icon" href="/favicon.ico" sizes="32x32" />
-                <link rel="mask-icon" href="icon.svg" color="#ffffff" />
+                <link rel="mask-icon" href="icon.svg" color="#000000" />
                 <link rel="icon" href="/icon.svg" type="image/svg+xml" />
                 <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
                 <link rel="manifest" href="/manifest.webmanifest" />
-
-                <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"/>
             </head>
             <body>
                 <MantineProvider
